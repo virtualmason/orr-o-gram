56b4656fd1b325e2d3e91c11b7381aad3b1597c2
Floyd <floyd_orr@comcast.com>
2021-05-27 14:21:49 -0600

    added database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Skeleton from "react-loading-skeleton";
+import { isBefore } from "date-fns";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+//Clieent rendered: app:react )ccra)
+   //databbase whiccch is firebase
+   //reeact-oading-sskelleton
+   //tailwind
+
+   //archiiteeccture
+    //components, 
+    //contant,
+    //context, 
+   // helpers,
+   // lib,
+   // services
+    //(firebase function in here)
+    //stylles (talwind's foldeer (app/tailwind()))
+    // ***lasst place 37mmin firebase
+    /**
+     Production rules when done, test lests us read and write to firestore(basically anything while in dev),need test mode for sead file to write to database at first
+rules_version = '2';
+
+service cloud.firestore {
+
+  match /databases/{database}/documents {
+
+    match /{document=**} {
+
+      allow read;
+      allow write : if request.auth.uid != null;
+
+        request.time < timestamp.date(2021, 6, 26);
+
+    }
+
+  }
+
+}
+
+     */
