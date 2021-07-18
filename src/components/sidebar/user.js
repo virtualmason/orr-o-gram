ea04306ffb27e377e97203508918c131d2fff95d
Floyd <floyd_orr@comcast.com>
2021-07-18 16:40:52 -0600

    added memo

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,26 +1,39 @@
-import PropTypes from 'prop-types';
-import { memo } from 'react'
-import Skeleton from 'react-loading-skeleton';
-import { Link } from 'react-router-dom'
-import { fromJSON } from 'postcss';
+import PropTypes from "prop-types";
+import { memo } from "react";
+import Skeleton from "react-loading-skeleton";
+import { Link } from "react-router-dom";
+import { fromJSON } from "postcss";
 
-const User = ({username, fullName}) => {
-    console.group(username, fullName)
- return !username || !fullName ? (
-        <Skeleton count={1} height={61} />
-
-    ):(
-        <Link to={`p/${username}`} className="grid" >
-            <p>{username}</p>
-        </Link>
-    )
-
-}
+const User = ({ username, fullName }) => {
+  console.group(username, fullName);
+  return !username || !fullName ? (
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link
+      to={`p/${username}`}
+      className="grid grid-cols-4 gap-4 mb-4 mb-6 items-center"
+      grid-cols-4
+      gap-4
+      mb-6
+      items-center
+    >
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/karl.jpeg`}
+        ></img>
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
+    </Link>
+  );
+};
 
 User.propTypes = {
-    username: PropTypes.string,
-    fullName: PropTypes.string
+  username: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired,
+};
 
-}
-
-export default User;
+export default memo(User);
